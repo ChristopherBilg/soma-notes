fff6ef7347e8626e72bba39b02b385e7dfa774a5
Chris Bilger <christopherbilg@gmail.com>
2023-06-09 18:46:19 +0000

    Domain name and github auth maybe

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,9 +6,11 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_500.tsx";
 import * as $2 from "./routes/_app.tsx";
-import * as $3 from "./routes/api/notes.ts";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/notes/[note].tsx";
+import * as $3 from "./routes/api/login.ts";
+import * as $4 from "./routes/api/logout.ts";
+import * as $5 from "./routes/api/notes.ts";
+import * as $6 from "./routes/index.tsx";
+import * as $7 from "./routes/notes/[note].tsx";
 import * as $$0 from "./islands/DoublePane.tsx";
 
 const manifest = {
@@ -16,9 +18,11 @@ const manifest = {
     "./routes/_404.tsx": $0,
     "./routes/_500.tsx": $1,
     "./routes/_app.tsx": $2,
-    "./routes/api/notes.ts": $3,
-    "./routes/index.tsx": $4,
-    "./routes/notes/[note].tsx": $5,
+    "./routes/api/login.ts": $3,
+    "./routes/api/logout.ts": $4,
+    "./routes/api/notes.ts": $5,
+    "./routes/index.tsx": $6,
+    "./routes/notes/[note].tsx": $7,
   },
   islands: {
     "./islands/DoublePane.tsx": $$0,
---
+++ helpers/github-auth.ts
@@ -0,0 +1,102 @@
+interface AccessTokenResponse {
+  ok: boolean;
+  error: string | null;
+  accessToken: string | null;
+}
+
+export const getAccessToken = async (
+  code: string,
+): Promise<AccessTokenResponse> => {
+  const response = await fetch(
+    "https://github.com/login/oauth/access_token",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        client_id: Deno.env.get("GITHUB_CLIENT_ID"),
+        client_secret: Deno.env.get("GITHUB_CLIENT_SECRET"),
+        code,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  if (!response.ok) {
+    return {
+      ok: false,
+      error: await response.text(),
+      accessToken: null,
+    };
+  }
+
+  const data = await response.json();
+  const accessToken = data?.["access_token"];
+  if (!accessToken) {
+    return {
+      ok: false,
+      error: "Access token not found in response.",
+      accessToken: null,
+    };
+  }
+
+  if (typeof accessToken !== "string") {
+    return {
+      ok: false,
+      error: "Access token is not a string.",
+      accessToken: null,
+    };
+  }
+
+  return {
+    ok: true,
+    error: null,
+    accessToken,
+  };
+};
+
+interface UserDataResponse {
+  ok: boolean;
+  error: string | null;
+  userId: number | null;
+  userName: string | null;
+  avatarUrl: string | null;
+}
+
+export const getUserData = async (
+  accessToken: string,
+): Promise<UserDataResponse> => {
+  const response = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: `token ${accessToken}`,
+    },
+  });
+  if (!response.ok) {
+    return {
+      ok: false,
+      error: await response.text(),
+      userId: null,
+      userName: null,
+      avatarUrl: null,
+    };
+  }
+
+  const userData = await response.json();
+  if (!userData) {
+    return {
+      ok: false,
+      error: "User data not found in response.",
+      userId: null,
+      userName: null,
+      avatarUrl: null,
+    };
+  }
+
+  return {
+    ok: true,
+    error: null,
+    userId: userData.id as number,
+    userName: userData.login as string,
+    avatarUrl: userData["avatar_url"] as string,
+  };
+};
