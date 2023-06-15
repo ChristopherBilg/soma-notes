9b48c2ab20cc8f025f9357e41215670ca47f0aa3
Chris Bilger <christopherbilg@gmail.com>
2023-06-15 17:04:44 +0000

    Standardizing more components

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,4 @@
-import { Handlers, PageProps } from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
 import { authHandler } from "../helpers/auth-handler.ts";
 import { UserDataResponse } from "../helpers/github-auth.ts";
 
@@ -14,25 +14,27 @@ export const handler: Handlers = {
   },
 };
 
-const LandingPage = (
-  { data }: PageProps<{ userData: UserDataResponse }>,
-) => {
+interface LandingPageProps {
+  data: { userData: UserDataResponse } | null;
+}
+
+const LandingPage = ({ data }: LandingPageProps) => {
   return (
     <div class="p-4 mx-auto max-w-screen-xlg">
       <h1 class="my-6 text-center">
         Welcome to{" "}
         <b>Soma Notes</b>, a simple, global, low-latency note keeping
-        application. You are {data?.userData?.userName || "not logged in"}!
+        application. You are {data?.userData.userName || "not logged in"}!
       </h1>
 
-      {data?.userData?.userId && (
+      {data?.userData.userId && (
         <h2 class="my-6 text-center">
           <a href="/notes">Notes</a>
         </h2>
       )}
 
       <h2 class="my-6 text-center">
-        {data?.userData?.userId
+        {data?.userData.userId
           ? <a href="/api/logout">Logout</a>
           : <a href="/api/login">Login</a>}
       </h2>
--- routes/notes/index.tsx
+++ routes/notes/index.tsx
@@ -1,4 +1,4 @@
-import { Handlers, PageProps } from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
 import { authHandler } from "../../helpers/auth-handler.ts";
 import { UserDataResponse } from "../../helpers/github-auth.ts";
 import DoublePane from "../../islands/DoublePane.tsx";
@@ -18,16 +18,20 @@ export const handler: Handlers = {
   },
 };
 
-const Notes = ({ data }: PageProps<{ userData: UserDataResponse }>) => (
+interface NotesProps {
+  data: { userData: UserDataResponse } | null;
+}
+
+const Notes = ({ data }: NotesProps) => (
   <div class="p-4 mx-auto max-w-screen-xlg">
     <h1 class="my-6 text-center">
       Welcome to{" "}
       <b>Soma Notes</b>, a simple, global, low-latency note keeping application.
-      You are {data?.userData?.userName || "not logged in"}!
+      You are {data?.userData.userName || "not logged in"}!
     </h1>
 
     <h2 class="my-6 text-center">
-      {data?.userData?.userId
+      {data?.userData.userId
         ? <a href="/api/logout">Logout</a>
         : <a href="/api/login">Login</a>}
     </h2>
