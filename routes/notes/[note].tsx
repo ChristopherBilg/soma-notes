d058a51dfc91dba6c73bf4590fcde10f63a3ebb0
Chris Bilger <christopherbilg@gmail.com>
2023-06-17 14:53:34 +0000

    Testing authentication on /notes/:note route

--- routes/notes/[note].tsx
+++ routes/notes/[note].tsx
@@ -1,7 +1,32 @@
-import { PageProps } from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
+import { authHandler } from "../../helpers/auth-handler.ts";
+import { UserDataResponse } from "../../helpers/github-auth.ts";
 
-const Note = (props: PageProps) => {
-  return <div>Note ID: {props.params.note}</div>;
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const response = await authHandler(
+      req,
+      ctx,
+      new Response("", {
+        status: 307,
+        headers: { Location: "/api/login" },
+      }),
+    );
+
+    return response;
+  },
+};
+
+interface NoteProps {
+  data: { userData: UserDataResponse } | null;
+  params: Record<string, string>;
+}
+
+const Note = ({ data, params }: NoteProps) => {
+  console.log("data", data);
+  console.log("params", params);
+
+  return <div>Note ID: {params.note}</div>;
 };
 
 export default Note;
