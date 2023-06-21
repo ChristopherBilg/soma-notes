270bbf8faf39b2d84fc0cc3691574301585d46bf
Chris Bilger <christopherbilg@gmail.com>
2023-06-21 16:20:35 +0000

    Cleaned up unnecessary auth logic

--- routes/index.tsx
+++ routes/index.tsx
@@ -33,7 +33,7 @@ const LandingPage = ({ data }: LandingPageProps) => (
           </>
         ) : (
           <a href="/api/login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Signup/Login
+            Login with GitHub
           </a>
         )}
       </div>
