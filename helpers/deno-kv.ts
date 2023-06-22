ca3eba0beafd937ca687c6b457f3427bcf474e37
Chris Bilger <christopherbilg@gmail.com>
2023-06-22 16:50:38 +0000

    Added future-proofing to KV for more auth methods

--- helpers/deno-kv.ts
+++ helpers/deno-kv.ts
@@ -1,8 +1,15 @@
 import { Note } from "./../signal/notes.ts";
 
+type AuthMethod = 'github';
+type DenoKVNotesKey = ["notes", AuthMethod, string];
+
+const getNotesKey = (authMethod: AuthMethod, userId: string): DenoKVNotesKey => {
+  return ["notes", authMethod, userId];
+};
+
 export const getNotesByUserId = async (userId: string): Promise<Note[]> => {
   const kv = await Deno.openKv();
-  const key = ["notes", userId];
+  const key = getNotesKey("github", userId);
 
   const notes = await kv.get(key);
   kv.close();
@@ -12,7 +19,7 @@ export const getNotesByUserId = async (userId: string): Promise<Note[]> => {
 
 export const setNotesByUserId = async (userId: string, notes: Note[]) => {
   const kv = await Deno.openKv();
-  const key = ["notes", userId];
+  const key = getNotesKey("github", userId);
 
   let res;
   if (notes.length === 0) await kv.delete(key);
