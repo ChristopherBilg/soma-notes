6a21f94b01943dc18cacecd60d93bdcfe5e16004
Christopher R. Bilger <christopherbilg@gmail.com>
2023-06-23 19:56:51 -0400

    Working on noteviewer

--- components/NoteViewer.tsx
+++ components/NoteViewer.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "preact/hooks";
-import { NotesContext } from "./Context.tsx";
+import { AuthContext, NotesContext } from "./Context.tsx";
 import NoteInput from "./NoteInput.tsx";
 
 interface NoteViewerProps {
@@ -7,13 +7,36 @@ interface NoteViewerProps {
 }
 
 const NoteViewer = ({ params }: NoteViewerProps) => {
-  const { notes } = useContext(NotesContext);
+  const { auth } = useContext(AuthContext);
+  const { notes, updateNote } = useContext(NotesContext);
+
   const note = notes.value.find((n) => n.uuid === params.note);
   if (!note) return null;
 
+  const handleNotePinButtonClick = () =>
+    updateNote(
+      auth?.value.userId || "",
+      note.uuid,
+      note.content,
+      note.pinned ? false : true,
+    );
+
   return (
     <div class="p-4 mx-auto max-w-screen-lg">
-      <div class="p-2.5 bg-gray-200 rounded-r-lg">
+      <div class="flex justify-evenly m-4">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleNotePinButtonClick}
+        >
+          {note.pinned ? "Unpin" : "Pin"}
+        </button>
+        
+        <a href={`/notes/${note.parent}`} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Parent
+        </a>
+      </div>
+
+      <div class="p-2.5 bg-gray-200 rounded-lg">
         <div>
           <ul class="list-disc ml-4">
             <li>
